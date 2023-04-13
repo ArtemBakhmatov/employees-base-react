b8c108b24aeaef5e996d6298fd9906e005655785
artem_bakhmarov <ip.bakhmatov@mail.ru>
2023-04-13 22:03:26 +0300

    Практика свойств на проекте(props)

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,17 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => { // тут нам приходит массив с объетками
+
+    const elements = data.map(item => { // весь массив перебираем (внутри объеткы)
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/> // 1 вариант
+            //<EmployeesListItem {...item}/> // 2 вариант // через spread оператор
+        )
+    })
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
