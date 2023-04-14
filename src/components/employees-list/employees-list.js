a994e6ac8b4423b26f0e0809231124c08d62816f
artem_bakhmarov <ip.bakhmatov@mail.ru>
2023-04-14 18:29:22 +0300

    Работа со списками и алгоритм согласования

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,11 +5,13 @@ import './employees-list.css';
 const EmployeesList = ({data}) => { // тут нам приходит массив с объетками
 
     const elements = data.map(item => { // весь массив перебираем (внутри объеткы)
+        //const {id, ...itemProps} = item; // для второго варианата
         return (
-            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/> // 1 вариант
-            //<EmployeesListItem {...item}/> // 2 вариант // через spread оператор
+            <EmployeesListItem key={item.id} name={item.name} salary={item.salary} increase={item.increase}/> // 1 вариант
+            //<EmployeesListItem key={id} {...itemProps}/> // 2 вариант // через spread оператор
         )
-    })
+    });
+
     return (
         <ul className="app-list list-group">
             {elements}
