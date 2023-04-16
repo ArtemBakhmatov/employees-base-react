a51d761198298e9aa79b233e335ca1ce44e87268
artem_bakhmarov <ip.bakhmatov@mail.ru>
2023-04-16 17:28:01 +0300

    Стили в React. CSS и SASSSCSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './components/app/app';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
-// вывод из этого урока: (работали с app.js и employees-list)
-// 1) реакт обновляет только те интерфейсы которые дейстивительно изменились
-// 2) в этом ему помогает алгоримт согласования и сравнивать старые компиии dom-дерева
-// 3) при работе со списком одинаковых сущностей используйте атрибут key (правильная работа алгоритма и ускорение
-// вашего приложения), чаще всего перебор в виде map
+// чтобы установить sass нужно установить npm пакет:
+// npm i sass
+
 
